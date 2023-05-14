a23f7cb2163c3bd1183a7f9c812ef569c77c52c3
Rhuvie Joyce Amarille <127023300+aeruby@users.noreply.github.com>
2023-05-14 11:16:06 +0800

    Add files via upload

---
+++ Reservation.cs
@@ -0,0 +1,55 @@
+﻿namespace TalisayTrackerClass
+{
+    public class Reservation
+    {
+        public int ReservationID { get; set; }
+        public DateTime CheckInDate { get; set; }
+        public DateTime CheckOutDate { get; set; }
+        public RoomType RoomType { get; set; }
+        public CottageType CottageType { get; set; }
+        public List<Amenities> Amenity { get; set; }
+        public Guest Guest { get; set; }
+        public bool IsPaid { get; set; }
+        public DateTime PaymentDate { get; set; }
+
+        public Reservation(int reservationID, DateTime checkInDate, DateTime checkOutDate,
+                           RoomType roomType, CottageType cottageType, List<Amenity> amenities,
+                           Guest guest, bool isPaid, DateTime paymentDate)
+        {
+            ReservationID = reservationID;
+            CheckInDate = checkInDate;
+            CheckOutDate = checkOutDate;
+            RoomType = roomType;
+            CottageType = cottageType;
+            Amenity = amenity;
+            Guest = guest;
+            IsPaid = isPaid;
+            PaymentDate = paymentDate;
+        }
+
+        public decimal GetTotalAmount()
+        {
+            decimal totalAmount = 0;
+
+            // Calculate room charges
+            totalAmount += RoomType.ChargePerTime * (decimal)(CheckOutDate - CheckInDate).TotalHours;
+
+            // Calculate cottage charges
+            totalAmount += CottageType.ChargePerTime * (decimal)(CheckOutDate - CheckInDate).TotalHours;
+
+            // Calculate amenities charges
+            foreach (Amenity amenity in Amenities)
+            {
+                totalAmount += amenity.Charge;
+            }
+
+            return totalAmount;
+        }
+
+        public void MarkAsPaid()
+        {
+            IsPaid = true;
+            PaymentDate = DateTime.Now;
+        }
+    }
+}
