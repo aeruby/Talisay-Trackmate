0e833ab9b83a11b5482d1f39a2b343b3d4742fc6
Rhuvie Joyce Amarille <127023300+aeruby@users.noreply.github.com>
2023-05-20 23:18:47 +0800

    classes

---
+++ TalisayTrackerClass/ReservationActivity.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TalisayTrackerClass
+{
+    public class ReservationActivity
+    {
+        private List<Reservation> reservations;
+
+        public ReservationActivity()
+        {
+            reservations = new List<Reservation>();
+        }
+
+        public void AddReservation(Reservation reservation)
+        {
+            reservations.Add(reservation);
+        }
+
+        public void RemoveReservation(Reservation reservation)
+        {
+            reservations.Remove(reservation);
+        }
+
+        public List<Reservation> GetReservations()
+        {
+            return reservations;
+        }
+
+        public List<Reservation> GetReservationsByDate(DateTime date)
+        {
+            return reservations.Where(r => r.CheckInDate.Date <= date.Date && r.CheckOutDate.Date >= date.Date).ToList();
+        }
+
+        public List<Reservation> GetReservationsByGuestName(string guestName)
+        {
+            return reservations.Where(r => r.Guest.FullName.ToLower().Contains(guestName.ToLower())).ToList();
+        }
+
+        public List<Reservation> GetReservationsByRoomType(RoomType roomType)
+        {
+            return reservations.Where(r => r.RoomType == roomType).ToList();
+        }
+
+        public List<Reservation> GetReservationsByCottageType(CottageType cottageType)
+        {
+            return reservations.Where(r => r.CottageType == cottageType).ToList();
+        }
+
+        public List<Reservation> GetReservationsByAmenity(Amenities amenity)
+        {
+            return reservations.Where(r => r.Amenities.Contains(amenity)).ToList();
+        }
+    }
+}
---
+++ TalisayTrackerClass/RoomType.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TalisayTrackerClass
+{
+    public class RoomType
+    {
+        public string Name { get; set; }
+        public decimal OvernightCharge { get; set; }
+        public decimal DaytimeCharge { get; set; }
+
+        public RoomType(string name, decimal overnightCharge, decimal daytimeCharge)
+        {
+            Name = name;
+            OvernightCharge = overnightCharge;
+            DaytimeCharge = daytimeCharge;
+        }
+
+        public decimal GetCharge(DateTime checkIn, DateTime checkOut)
+        {
+            decimal totalCharge = 0;
+
+            // Calculate overnight charges (3 pm - 12 pm of the next day)
+            if (checkIn.Hour >= 15 && checkOut.Hour <= 12 && checkOut.Date == checkIn.Date.AddDays(1))
+            {
+                totalCharge = OvernightCharge * (decimal)(checkOut.Date - checkIn.Date).TotalDays;
+            }
+            // Calculate daytime charges (6 am - 12 pm)
+            else if (checkIn.Hour >= 6 && checkOut.Hour <= 12 && checkOut.Date == checkIn.Date)
+            {
+                totalCharge = DaytimeCharge * (decimal)(checkOut.Date - checkIn.Date).TotalDays;
+            }
+
+            return totalCharge;
+        }
+    }
+
+}
